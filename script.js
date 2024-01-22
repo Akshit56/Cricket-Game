5400191f097fc780432cc86812d4685228b12e06
Akshit Gupta <akshit0689@gmail.com>
2024-01-22 20:04:22 +0530

    Using functions

--- script.js
+++ script.js
@@ -11,14 +11,17 @@ function randomNumber() {
 
 function getResult(userMove, computerMove) {
   if (userMove === computerMove) {
+    score.tie++;
     return `It's a tie`;
   } else if (
     (userMove === "Bat" && computerMove === "Ball") ||
     (userMove === "Ball" && computerMove === "Stump") ||
     (userMove === "Stump" && computerMove === "Bat")
   ) {
+    score.win++;
     return `User Won`;
   } else {
+    score.lost++;
     return `Computer Won`;
   }
 }
@@ -26,7 +29,17 @@ function getResult(userMove, computerMove) {
 function playGame(userChoice) {
   let computerChoice = randomNumber();
   let resultMsg = getResult(userChoice, computerChoice);
+  localStorage.setItem('Score', JSON.stringify(score));
   alert(
-    `You chose ${userChoice}\nComputer chose ${computerChoice}\nResult: ${resultMsg}`
+    `You chose ${userChoice}\nComputer chose ${computerChoice}\nResult: ${resultMsg}\n${score.displayScore()}`
   );
 }
+
+let score ={
+  win:0,
+  tie:0,
+  lost:0,
+  displayScore(){
+    return `Won ${score.win} Tie ${score.tie} Lost ${score.lost}`
+  }
+};
